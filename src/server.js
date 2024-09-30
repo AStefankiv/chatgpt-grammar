e62802987854a7c28a7fe9feb3213d4c12dbd49b
AStefankiv <stefankif35@gmail.com>
2024-09-29 18:16:32 -0600

    Commit before big changes

--- src/server.js
+++ src/server.js
@@ -1,45 +1,3 @@
-// import { config } from "dotenv";
-// config();
-// import { OpenAIApi, Configuration } from "openai";
-// import readline from "readline";
-
-
-// if (!process.env.API_KEY) {
-//   console.error('API_KEY is required');
-//   process.exit(1);
-// }
-
-
-// const configuration = new Configuration({
-//   apiKey: process.env.API_KEY
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// const userInterface = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// userInterface.prompt();
-
-// userInterface.on('line', async (input) => {
-//   try {
-//     const response = await openai.createChatCompletion({
-//       model: 'gpt-3.5-turbo',
-//       messages: [{ role: 'user', content: input }]
-//     });
-//     console.log(response.data.choices[0].message.content);
-//   } catch (error) {
-//     console.error(error);
-//   }
-
-//   userInterface.prompt();
-// });
-
-
-
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import { config } from 'dotenv';
@@ -73,6 +31,7 @@ app.use(express.static(path.join(__dirname, '..', 'frontend')));
 // API endpoint to handle chat messages
 app.post('/api/chat', async (req, res) => {
   const { message } = req.body;
+  console.log('Message:', message);
 
   try {
     const response = await openai.createChatCompletion({
